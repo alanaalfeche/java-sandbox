11baecaf2faacaabdaa717a1db0b43c888abaed6
Alana Alfeche <alanaalfeche@gmail.com>
2019-12-24 21:24:06 -0600

    HWK 3 Start

---
+++ 6.092 /Marathon.java
@@ -0,0 +1,26 @@
+
+import.java.util.ArrayList;
+
+class Marathon{
+
+    private static int[] sort(int[] times){
+        // learning sorting algorithms first
+        return times;
+    }
+
+    public static void main(String[] arguments){
+        String[] names = {
+            "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex",
+            "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda",
+            "Aaron", "Kate"
+        };
+
+        int[] times = {
+            341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299,
+            343, 317, 265
+        };
+        
+        sort(times);
+    }
+    
+}
