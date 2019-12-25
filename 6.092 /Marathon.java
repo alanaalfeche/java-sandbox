fa755e7ebbf486a8854acc9c7d535f05b91d310c
Alana Alfeche <alanaalfeche@gmail.com>
2019-12-25 15:15:06 -0600

    HWK 3 Complete

--- 6.092 /Marathon.java
+++ 6.092 /Marathon.java
@@ -1,14 +1,27 @@
-
-import.java.util.ArrayList;
+import java.util.Arrays;
 
 class Marathon{
 
-    private static int[] sort(int[] times){
-        // learning sorting algorithms first
-        return times;
+    // Selection Sort
+    private static int[] sort(int[] input){
+        for(int i = 0; i < input.length-1; i++){
+            int minIndex = i;
+
+            for(int j = i + 1; j < input.length; j++){
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = input[minIndex];
+            input[minIndex] = input[i];
+            input[i] = temp;
+        }
+
+        return input;
     }
 
-    public static void main(String[] arguments){
+    public static void main(String[] args){
         String[] names = {
             "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex",
             "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda",
@@ -20,7 +33,29 @@ class Marathon{
             343, 317, 265
         };
         
-        sort(times);
+        int[] timesCopy = Arrays.copyOf(times, times.length);
+
+        sort(timesCopy);
+    
+        int fastestTime = timesCopy[timesCopy.length - 1];
+        int secondFastedTime = timesCopy[timesCopy.length - 2];
+        
+        int fastestTimeIndex, secondFastestTimeIndex;
+        fastestTimeIndex = secondFastestTimeIndex = 0;
+
+        for (int i = 0; i < times.length - 1; i++) {
+            if (times[i] == fastestTime) {
+                fastestTimeIndex = i;
+            } 
+            
+            if (times[i] == secondFastedTime) {
+                secondFastestTimeIndex = i;
+            }
+        }
+
+        System.out.println("First Place: " + names[fastestTimeIndex] + " at " + fastestTime + " minutes");
+        System.out.println("Second Place: " + names[secondFastestTimeIndex] + " at " + secondFastedTime + " minutes");
+ 
     }
     
 }
